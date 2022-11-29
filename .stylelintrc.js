97a2303f120220e7daf417812d978e140b05ac8f
drashtid-7span <drashtid-7span>
2022-11-29 17:05:14 +0530

    dist added

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "plugins": ["stylelint-scss"],
+  "defaultSeverity": "warning",
+  "ignoreFiles": ["*/.js"],
   "rules": {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
